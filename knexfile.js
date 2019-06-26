d050845af992fa14bc671fc55f1833b5ca7219dd
MAllen07 <mrsmallen07@gmail.com>
2019-06-26 15:06:42 -0400

    added another object literal to file

--- knexfile.js
+++ knexfile.js
@@ -16,5 +16,20 @@ module.exports = {
     seeds: {
       directory: "./db/seeds"
     }
+  },
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
   }
 };
