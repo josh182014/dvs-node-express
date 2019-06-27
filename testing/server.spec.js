feb3215f5ad02a68ebfff25c61fab932149619b7
MAllen07 <mrsmallen07@gmail.com>
2019-06-27 15:27:22 -0400

    testing and fixing errors

--- testing/server.spec.js
+++ testing/server.spec.js
@@ -12,22 +12,34 @@ describe("GET/api", () => {
 
 
 
-  
-//GET endpoint tested - passed!
+
+  //GET endpoint tested - passed!
   it('returns 200 OK', () => {
     return request(server).get('/')
       .expect(200)
   }
   );
+//GET - passed
+  it("GET/users should return an array", () => {
+    const expected = [];
 
-  //POST Endpoint
-  // it("returns 201", () => {
-  //   return request(server)
-  //     .post("/register")
-  //     .send({ message: "welcome" })
-  //     .then(res => {
-  //       expect(res.status).toBe(201);
-  //     });
-  // });
-  
+    return request(server)
+      .get("/users")
+      .then(res => {
+        expect(expected).toEqual([]);
+      });
+  });
+
+// POST - 
+  it("POST/users should return code:200", () => {
+    return request(server)
+      .post("/users")
+      .send({
+        message: `Welcome ${user.username}!` })
+      .then(res => {
+        expect(res.status).toBe(200).json;
+      });
+
+
+  })
 });
