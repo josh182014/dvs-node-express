1c0f3d5c8cdc7be52ddbb0b9c2aa2185b8be9efe
MAllen07 <mrsmallen07@gmail.com>
2019-06-26 20:27:57 -0400

    correctly production structure

--- knexfile.js
+++ knexfile.js
@@ -21,26 +21,23 @@ module.exports = {
     client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './data/migrations'
+      directory: "data/migrations"
     },
+    seeds: {
+      directory: "data/seeds"
+    }
   }
-}
+};
 
-
-
-
-
-
-
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password"
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations"
-  //   }
-  // }
+//     database: "my_db",
+//     user: "username",
+//     password: "password"
+//   },
+//   pool: {
+//     min: 2,
+//     max: 10
+//   },
+//   migrations: {
+//     tableName: "knex_migrations"
+//   }
+// }
