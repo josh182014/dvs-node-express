2b295f3522000a4d079d7c16d965eddf6e51c40c
MAllen07 <mrsmallen07@gmail.com>
2019-06-27 14:54:33 -0400

    testing endpoints

--- testing/server.spec.js
+++ testing/server.spec.js
@@ -1,9 +1,24 @@
 const server = require("../api/server");
 const request = require("supertest");
+require("dotenv").config();
 
 describe("GET/api", () => {
-  //Sanity check to see if the env variables are set
-  it('has process.env.DB_ENV as "testing', () => {
-    expect(process.env.DB_ENV).toBe("testing");
+  //Sanity check to see if the env variables are set.. passed!
+  it('has process.env.NODE_ENV as "test', () => {
+    expect(process.env.NODE_ENV).toBe("test");
   });
+//tested - passed!
+  it('returns 200 OK', () => {
+    return request(server).get('/')
+      .expect(200)
+  }
+  );
+
+  //new registration - 
+
+  it('returns 201 newUser', () => {
+    return request(server).get('/api/register')
+    .expect(201)
+  }
+  )
 });
