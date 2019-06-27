2cc020425c69f23cd2a9630dbada7b94df687a6d
MAllen07 <mrsmallen07@gmail.com>
2019-06-26 20:59:21 -0400

    changed db location for production

--- knexfile.js
+++ knexfile.js
@@ -21,10 +21,10 @@ module.exports = {
     client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: "data/migrations"
+      directory: "./db/migrations"
     },
     seeds: {
-      directory: "data/seeds"
+      directory: "./db/seeds"
     }
   }
 };
