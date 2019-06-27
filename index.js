93d25270cca4cf860c1abd03f0132e46fe8c749a
MAllen07 <mrsmallen07@gmail.com>
2019-06-26 21:01:45 -0400

    changed production port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // includes server and setup for routes
 
-require('dotenv').config();
+require("dotenv").config();
 
-const server = require('./Api/server');
+const server = require("./Api/server");
 
-const port = process.env.port || 4004; // dynamic port 
+const port = process.env.PORT || 4004; // dynamic port
 server.listen(port, () => console.log(`\n** server up on port ${port} **\n`));
